c4bcc4e68ec5a4ac5a0128d5ba796b0591a7208f
reem <a.reem@elag.sa>
2021-11-11 11:32:33 +0300

    set up composition api, router, and vuetify

--- movie-listing-proj/src/main.js
+++ movie-listing-proj/src/main.js
@@ -1,8 +1,21 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
+import VueCompositionAPI from '@vue/composition-api'
 import vuetify from "./plugins/vuetify";
+import VueRouter from "vue-router";
 
+import routes from "./router"
+
+Vue.use(VueCompositionAPI);
+Vue.use(VueRouter);
+
+
+const router = new VueRouter({
+  routes: routes,
+  mode: "history",
+  base: process.env.BASE_URL,
+});
+ 
 Vue.config.productionTip = false;
 
 new Vue({
--- movie-listing-proj/src/router/index.js
+++ movie-listing-proj/src/router/index.js
@@ -1,30 +1,17 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Movie from "../views/Movie.vue";
 
-Vue.use(VueRouter);
 
-const routes = [
+export default [
   {
     path: "/",
     name: "Home",
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+    path: "/Movie/:movieId",
+    name: "Movie",
+    component: Movie,
+    props: true
+  }
 ];
-
-const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes,
-});
-
-export default router;
